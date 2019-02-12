bb2ccc0df92b44ef7e5a73bd3f220573bc701a7c
Huy Nguyen <huy.nguyen@act-on.net>
2019-02-11 16:12:03 -0800

    added tests used Enzyme and react-renderer

--- client/src/test/bookList.spec.js
+++ client/src/test/bookList.spec.js
@@ -1,6 +1,4 @@
 import React from "react";
-import renderer from "react-test-renderer";
-import BookCreate from "../components/books/BookCreate";
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
 
@@ -8,6 +6,7 @@ import { shallow } from "enzyme";
 
 import Adapter from "enzyme-adapter-react-16";
 import Enzyme from "enzyme";
+import BookList from "../components/books/BookList";
 
 const mockStore = configureMockStore();
 const store = mockStore({});
@@ -17,24 +16,18 @@ Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true });
 
 //use jest -u to update the snapshot
 //using react-render + jest snapshot feature
-test("Book Create component", () => {
-  const tree = renderer
-    .create(
-      <Provider store={store}>
-        <BookCreate />
-      </Provider>
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-//use enzyme shallow render
-test("Book create component ", () => {
-  const bookCreateComponent = shallow(
+test("Books Listing header", () => {
+  const bookListComponent = shallow(
     <Provider store={store}>
-      <BookCreate />
+      <BookList />
     </Provider>
   );
 
-  expect(bookCreateComponent.find("BookForm")).toBeTruthy();
+  expect(bookListComponent.children('h2').text()).toEqual('Books');
+
+  //const header = bookListComponent.children('div h2');
+  //const header = bookListComponent.find("div h2");
+  // const header = bookListComponent.find('h2');
+  // expect(header.text()).toEqual('Books');
+
 });
